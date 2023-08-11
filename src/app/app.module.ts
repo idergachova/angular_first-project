ad65001cb48a58c65a8e0b3f3acaa3b7eb734e60
idergachova <dergachovaira@gmail.com>
2023-08-11 15:46:11 +0200

    first step in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderUserComponent } from './header-user/header-user.component';
+import { FooterUserComponent } from './footer-user/footer-user.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderUserComponent,
+    FooterUserComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
